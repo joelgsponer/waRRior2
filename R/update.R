7f94a1b7b748a8f6fbccda4ea7771394570214e7
Joel Gsponer <jgsponer@gmail.com>
2019-03-20 11:50:33 +0100

    added "" around waRRior

--- R/update.R
+++ R/update.R
@@ -6,6 +6,6 @@
 #' @examples
 self_update <- function(){
   waRRior::print_if_verbose("Initiating self update", T)
-  update_packages(waRRior)
+  update_packages("waRRior")
   waRRior::print_if_verbose("Done", T)
 }
